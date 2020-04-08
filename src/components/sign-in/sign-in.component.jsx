cb56ea56e2722a7f0f46f7f1063a299c403ccb6a
Pawel Koper <pawel.koper@new-gravity.com>
2020-04-08 13:28:24 +0200

    add sign out functionality

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -19,8 +19,8 @@ class SignIn extends React.Component {
     };
 
     handleChange = event => {
-      const {name, value} = event.target;
-      this.setState({ [name]: value})
+        const {name, value} = event.target;
+        this.setState({[name]: value})
     };
 
     render() {
@@ -28,13 +28,18 @@ class SignIn extends React.Component {
                 <h2>I already have an account</h2>
                 <span>Sign in with your email and password</span>
                 <form onSubmit={this.handleSubmit}>
-                    <FormInput name='email' type='email' value={this.state.email} label='email' handleChange={this.handleChange} required />
-                    <FormInput name='password' type='password' value={this.state.password} label='password' handleChange={this.handleChange} required />
-                    <CustomButton type='submit' >Sign In</CustomButton>
-                    <CustomButton onClick={signInWithGoogle} >Sign In With Google</CustomButton>
+                    <FormInput name='email' type='email' value={this.state.email} label='email'
+                               handleChange={this.handleChange} required/>
+                    <FormInput name='password' type='password' value={this.state.password} label='password'
+                               handleChange={this.handleChange} required/>
+                    <div className='buttons'>
+                        <CustomButton type='submit'>Sign In</CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign In With Google</CustomButton>
+                    </div>
                 </form>
             </div>
         )
     }
 }
+
 export default SignIn;
