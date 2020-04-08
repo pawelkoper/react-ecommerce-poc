a5c275c4c5e219f61a44a6bb952dabb6f6f71952
Pawel Koper <pawel.koper@new-gravity.com>
2020-04-08 10:50:49 +0200

    add header component

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -6,7 +6,6 @@ const CollectionItem = ({id, name, price, imageUrl}) => (
 
     <div className='collection-item'>
         <div className='image' style={{backgroundImage: `url(${imageUrl})`}}/>
-
         <div className='collection-footer'>
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
